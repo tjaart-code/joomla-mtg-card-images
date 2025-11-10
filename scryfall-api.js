53692b8db12a5aee1b07f69cba7a7e95aaf1c03e
tjaart-code <184257127+tjaart-code@users.noreply.github.com>
2025-11-10 05:52:52 +0200

    Update tooltip to persist on mouse leave
    Modified tooltip behavior to persist on mouse leave.

--- scryfall-api.js
+++ scryfall-api.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
     $(".card-link").hover(function () {
-      var cardName = $(this).data("card-name");
-      var apiUrl = "https://api.scryfall.com/cards/named?fuzzy=" + encodeURIComponent(cardName);
+        // Mouse enter - fetch and show tooltip
+        var cardName = $(this).data("card-name");
+        var apiUrl = "https://api.scryfall.com/cards/named?fuzzy=" + encodeURIComponent(cardName);
 
-      $.get(apiUrl, function (data) {
-        // Handle the API response and display card information in the tooltip
-        var cardTooltip = $(".card-tooltip");
-        cardTooltip.find(".card-tooltip-content").html(
-          "<img src='" + data.image_uris.normal + "' alt='" + cardName + "'>"
-        ); // Removed the paragraph with card text
-        cardTooltip.show();
-      });
-    }, function () {
-      // Hide the tooltip when the user moves the mouse away
-      $(".card-tooltip").hide();
+        $.get(apiUrl, function (data) {
+            var cardTooltip = $(".card-tooltip");
+            cardTooltip.find(".card-tooltip-content").html(
+                "<img src='" + data.image_uris.normal + "' alt='" + cardName + "'>"
+            );
+            cardTooltip.show();
+        });
+    }, 
+    function () {
+        // Mouse leave - do NOT hide the tooltip (remove this if you want it to persist)
+        // Optional: Add a delay before hiding (if needed)
+        // setTimeout(() => $(".card-tooltip").hide(), 1000);
     });
 });
